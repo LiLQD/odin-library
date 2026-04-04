c811b3038b3964b8b3a6430ab4dd45d6e256b1e5
LiLQD <luuquangdung1511@gmail.com>
2026-04-05 01:50:08 +0700

    Make web layout

--- script.js
+++ script.js
@@ -8,27 +8,22 @@ function Book(id, title, author, pages, read) {
 	this.read = read;
 	this.info = function () {
 		if (read === false)
-			console.log(`${this.name} by ${this.author}, ${this.pages} pages, not read yet`);
+			console.log(`${this.id} ${this.name} by ${this.author}, ${this.pages} pages, not read yet`);
 	}
 }
 
-function addBookToLibrary(n) {
-	
-	for(let i = 0; i < n; i++){
-		let title = prompt("Title");
-		let author = prompt("Author");
-		let pages = prompt("Pages:");
-		let read = prompt("Read ? (True or false)");
-		const newBook = new Book(title, author, pages, read);
-		myLibrary.push(newBook);
-	}
-
+function addBookToLibrary() {
+	let id = crypto.randomUUID();
+	let title = prompt("Title");
+	let author = prompt("Author");
+	let pages = prompt("Pages:");
+	let read = prompt("Read ? (True or false)");
+	const newBook = new Book(id, title, author, pages, read);
+	myLibrary.push(newBook);
 }
-function Print(){
-	for(let i = 0; i < n; i++){
+function Print() {
+	for (let i = 0; i < n; i++) {
 		myLibrary[i].info();
 	}
 }
-let n = prompt("Nhap so luong");
-addBookToLibrary(n);
-Print();
+
