9f0bacc9dd679ffdc6c46329b1c67721aba1251e
LiLQD <luuquangdung1511@gmail.com>
2026-04-11 18:27:32 +0700

    Add delete button

--- script.js
+++ script.js
@@ -21,18 +21,27 @@ function addBookToLibrary() {
 	myLibrary.push(newBook);
 	console.log(newBook.info());
 }
-function Print() {
-	for (let i = 0; i < myLibrary.length; i++) {
-		myLibrary[i].info();
-	}
+function addCardToPage() {
+	const card = document.createElement("div");
+	cardContainer.appendChild(card)
 }
+
+const cardContainer = document.querySelector(".card-container");
+
 
 const openButton = document.querySelector("#add-btn");
 const closeButton = document.querySelector(".close-dialog");
-// const saveButton = document.querySelector(".save-book")
-
+const saveButton = document.querySelector(".save-book");
+const deleteButton = document.querySelectorAll(".remove");
 const addBookDialog = document.querySelector('#add-dialog');
 
+deleteButton.forEach(button => {
+	button.addEventListener('click', () => {
+		this.parentNode.parentNode.parentNode.removeChild(this.parentNode);
+	})
+});
+
+saveButton.addEventListener('click', addCardToPage());
 openButton.addEventListener('click', () => {
 	addBookDialog.showModal();
 })
